e750d798d6b415429712b960e95f0bda6a92c1f7
DongHyun(Daniel) <d.lee77668@gmail.com>
2025-01-20 23:41:41 -0500

    Initial Commit
    Added CardSO file

---
+++ Assets/Scripts/SO/CardSO.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+// Enum for CardType
+public enum ECardType {Gem, Relic, Spell};
+
+// Enum for CardCost
+public enum ECardCost{c1, c2, c3, c4,c5,c6,c7,c8};
+
+[System.Serializable]
+public class Card
+{
+    // Card info (name, type, cost, description, image)
+    public string name;
+    public ECardType eCardType;
+    public ECardCost eCarCost;
+    public string description;
+    public Sprite image;
+    
+    // For game mode (Normal, 1.5x, 2.0x)
+    public int carNum;
+
+}
+    
+
+[CreateAssetMenu(fileName = "CardSO", menuName = "Scriptable Objects/CardSO")]
+public class CardSO : ScriptableObject
+{
+    public Card[] cards;    
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Aeon-s-End-Shuffler
  A shuffle software for Aeon's End
+
+
+ ## Todo List
+
+ # Card setting
+ - Set up properties with Card Info( name, type, cost, description, image)
+ # Supply Card Deck
+ - Fill the supply card deck with Cards
+ # Game Mode 
+ # UI (GameMode, random cost, Extension Version, Result, Restart)
