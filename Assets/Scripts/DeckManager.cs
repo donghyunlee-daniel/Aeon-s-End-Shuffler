7a296fef85249d36562689b756e4ff0405ac738c
DongHyun(Daniel) <d.lee77668@gmail.com>
2025-02-04 21:56:48 -0500

    Clean some codes and write down todo lists for the new algorithm

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 
 public class DeckManager : MonoBehaviour
 {
-    public static DeckManager Inst {get;private set;}
+    public static DeckManager Inst { get; private set; }
     void Awake() => Inst = this;
 
 
@@ -25,19 +25,27 @@ public class DeckManager : MonoBehaviour
 
     const string URL = "https://docs.google.com/spreadsheets/d/1vTKQfIfWiSmaHZBsgp7q1rVpm4O8NUvD639-jLm60s4/export?format=tsv";
 
- 
-    private void OnDestroy() {
+
+    private void OnDestroy()
+    {
         cardSO.cards = null;
     }
 
+    // Prompt users to choose cards each list (gems, spells, relics)
+    // UI pops up and select cards
+    // At the final UI, show users selected cards
+    // Needed for panel. 
     
+    // Checkbox for boost mode
+    // 
 
-  
+
+
 
     void GenerateCard()
     {
         // float yVal = 490f;     
-        
+
         // foreach(var cardItem in cardList)
         // {
         //     var cardObject = cardPrefab.transform.GetChild(0).gameObject;
@@ -51,12 +59,12 @@ public class DeckManager : MonoBehaviour
         //Able to instantiate  card from prefab. Need to instantiate as many as the size of carlist
     }
 
-   
+
 
     void shuffleCardDeck(List<Item> deck)
     {
         // Randomize the card order
-        for(int i=0; i < deck.Count; i++)
+        for (int i = 0; i < deck.Count; i++)
         {
             int rand = Random.Range(i, deck.Count);
             Item temp = deck[i];
@@ -68,9 +76,9 @@ public class DeckManager : MonoBehaviour
     public IEnumerator cardSetUpCo()
     {
         UnityWebRequest www = UnityWebRequest.Get(URL);
-            yield return www.SendWebRequest();
-            string data= www.downloadHandler.text;
-            SetCardDeck(data);
+        yield return www.SendWebRequest();
+        string data = www.downloadHandler.text;
+        SetCardDeck(data);
     }
 
     void SetCardDeck(string tsv)
@@ -80,79 +88,95 @@ public class DeckManager : MonoBehaviour
         int colSize = row[0].Split('\t').Length;
 
         // counter value for do-while loop
-        int counter =0;
+        int counter = 0;
 
         //Initialize card list with the number of cards
         cardSO.cards = new Item[rowSize];
         gemList = new List<Item>();
         relicList = new List<Item>();
         spellList = new List<Item>();
-        for(int i =0; i <rowSize; i++)
+        for (int i = 0; i < rowSize; i++)
         {
             string[] col = row[i].Split('\t');
-            do{
+            do
+            {
                 Item targetCard = new Item();
 
                 // Assign name
                 targetCard.name = col[counter++];
 
                 // Assign cardType
-                switch(col[counter++].ToUpper())
+                switch (col[counter++].ToUpper())
                 {
-                    case "GEM" : targetCard.eCardType = ECardType.Gem;
-                    break;
-                    case "RELIC" : targetCard.eCardType = ECardType.Relic;
-                    break;
-                    case "SPELL" : targetCard.eCardType = ECardType.Spell;
-                    break;
+                    case "GEM":
+                        targetCard.eCardType = ECardType.Gem;
+                        break;
+                    case "RELIC":
+                        targetCard.eCardType = ECardType.Relic;
+                        break;
+                    case "SPELL":
+                        targetCard.eCardType = ECardType.Spell;
+                        break;
                     default:
-                    break;
+                        break;
                 }
 
                 // Assign cardCost
-                switch(col[counter++].ToString())
+                switch (col[counter++].ToString())
                 {
-                    case "0": targetCard.eCarCost = ECardCost.c0;
-                    break;
-                    case "1": targetCard.eCarCost = ECardCost.c1;
-                    break;
-                    case "2": targetCard.eCarCost = ECardCost.c2;
-                    break;
-                    case "3": targetCard.eCarCost = ECardCost.c3;
-                    break;
-                    case "4": targetCard.eCarCost = ECardCost.c4;
-                    break;
-                    case "5": targetCard.eCarCost = ECardCost.c5;
-                    break;
-                    case "6": targetCard.eCarCost = ECardCost.c6;
-                    break;
-                    case "7": targetCard.eCarCost = ECardCost.c7;
-                    break;
-                    case "8": targetCard.eCarCost = ECardCost.c8;
-                    break;
+                    case "0":
+                        targetCard.eCarCost = ECardCost.c0;
+                        break;
+                    case "1":
+                        targetCard.eCarCost = ECardCost.c1;
+                        break;
+                    case "2":
+                        targetCard.eCarCost = ECardCost.c2;
+                        break;
+                    case "3":
+                        targetCard.eCarCost = ECardCost.c3;
+                        break;
+                    case "4":
+                        targetCard.eCarCost = ECardCost.c4;
+                        break;
+                    case "5":
+                        targetCard.eCarCost = ECardCost.c5;
+                        break;
+                    case "6":
+                        targetCard.eCarCost = ECardCost.c6;
+                        break;
+                    case "7":
+                        targetCard.eCarCost = ECardCost.c7;
+                        break;
+                    case "8":
+                        targetCard.eCarCost = ECardCost.c8;
+                        break;
                     default:
-                    break;
+                        break;
                 }
 
                 // Assign description
-                targetCard.description = col[counter++]; 
-                
-               
+                targetCard.description = col[counter++];
+
+
                 // Add to the array
                 cardSO.cards[i] = targetCard;
-                switch(targetCard.eCardType.ToString())
+                switch (targetCard.eCardType.ToString())
                 {
-                    case "Gem" : gemList.Add(targetCard);
-                    break;
-                    case "Relic" : relicList.Add(targetCard);
-                    break;
-                    case "Spell" : spellList.Add(targetCard);
-                    break;
+                    case "Gem":
+                        gemList.Add(targetCard);
+                        break;
+                    case "Relic":
+                        relicList.Add(targetCard);
+                        break;
+                    case "Spell":
+                        spellList.Add(targetCard);
+                        break;
                     default:
-                    break;
+                        break;
                 }
-            }while(counter < colSize);
-            counter =0;
+            } while (counter < colSize);
+            counter = 0;
         }
         shuffleCardDeck(gemList);
         shuffleCardDeck(spellList);
@@ -162,7 +186,7 @@ public class DeckManager : MonoBehaviour
         print(relicList.Count);
         GenerateCard();
     }
-    
 
-    
+
+
 }
